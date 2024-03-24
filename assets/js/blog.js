21d98c2d7a32c22ef398f88deda94fdb2f3b769b
Owen Kanzler <oj.kanz1@gmail.com>
2024-03-23 19:08:52 -0500

    commented new javascript

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,14 +6,19 @@ const blogPostsSection = document.querySelector(".blog-posts");
 // gets the existing array of blogposts or creates an empty array
 const existingPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
 
-// loops through each post in the array
+// checks to see if the array of posts is empty or not
 if (existingPosts.length === 0) {
+  // creates a h2 with the class "no-posts"
   const noPosts = document.createElement("h2");
   noPosts.classList.add("no-posts");
 
+  // adds the h2 to the html
   noPosts.innerHTML = "<h2>Sorry There Are No Posts Right Now</h2>";
+
+  // appends it to the blog section
   blogPostsSection.append(noPosts);
 } else {
+  // loops through each post in the array
   existingPosts.forEach((post) => {
     // creates a div with the class name "post"
     const newPostDiv = document.createElement("div");
